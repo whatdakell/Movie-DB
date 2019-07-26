723fe7cbb57347e38b610a90fff528103238d290
Kelly <kellylinehan2@gmail.com>
2019-07-26 08:44:57 -0700

    working copy

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,6 +1,7 @@
 console.log('\'Allo \'Allo!');
 $(document).ready(function () {
-  // Limit Helper to only include top 5 for HB
+  Handlebars.registerPartial('gallery-item', Handlebars.templates['gallery-item.hbs']); // Limit Helper to only include top 5 for HB
+
   Handlebars.registerHelper('limit', function (arr, limit) {
     if (!Array.isArray(arr)) {
       return [];
@@ -16,32 +17,72 @@ $(document).ready(function () {
   function bind() {
     $(document).on('click', '.btn', function () {
       var d = $(this).data('key');
-      renderData(d, 'detail');
+      var $self = $(this);
+      var $container = $self.closest('.content-box');
+      renderData($container, d, 'detail');
     });
     $(document).on('click', '.goBack', function () {
-      renderData('popular', 'gallery');
+      console.log(this);
+      var d = $(this).data('key'); // console.log(d);
+
+      var $self = $(this);
+      var $container = $self.closest('.content-box');
+      renderData($container, d, 'gallery-item'); // var popularCacheJSON = sessionStorage.getItem('PopularCacheAPI');
+      // var popularCache = JSON.parse(popularCacheJSON);
+      // var popularCacheJSON = sessionStorage.getItem('PopularCacheAPI');
+      // var popularCache = JSON.parse(popularCacheJSON);
+      //
+      //
+      // var filteredID = popularCache.filter(filterByID);
     });
   }
 
-  function renderData(type, temp) {
-    $.ajax({
-      type: 'GET',
-      url: `https://api.themoviedb.org/3/movie/${type}?api_key=${api_key}&append_to_response=videos&language=en-US&page=1`,
-      success: function (data) {
-        if (type === 'popular') {}
+  function renderData(selector, type, temp) {
+    var popularCacheJSON = sessionStorage.getItem('popCache');
+    var popularCache = JSON.parse(popularCacheJSON);
+    console.log('pop', popularCache);
 
-        populardata = data;
-        console.log('data', data);
-        var template = Handlebars.templates[temp + '.hbs'](data);
-        $("#content-holder").html('').html(template);
-      },
-      error: function (XMLHttpRequest) {
-        console.log('error', XMLHttpRequest);
-      }
-    });
+    if (temp == 'gallery-item') {
+      var filteredID = popularCache.filter(function (element) {
+        return filterByID(element, type);
+      });
+      console.log(filteredID[0]);
+      var template = Handlebars.templates[temp + '.hbs'](filteredID[0]);
+      $(selector).html('').html(template);
+    } else {
+      $.ajax({
+        type: 'GET',
+        url: `https://api.themoviedb.org/3/movie/${type}?api_key=${api_key}&append_to_response=videos&language=en-US&page=1`,
+        success: function (data) {
+          if (type === 'popular') {
+            console.log('data', data);
+            popCache = data.results;
+            sessionStorage.setItem('popCache', JSON.stringify(popCache));
+          } // if (data.results){
+          // 	var resultsArray = data.results;
+          // 	sessionStorage.setItem('resultsArray', resultsArray);
+          // 	var filteredID = resultsArray.filter(filterByID);
+          // 	console.log('filtered',filteredID);
+          // }
+
+
+          var template = Handlebars.templates[temp + '.hbs'](data);
+          $(selector).html('').html(template);
+        },
+        error: function (XMLHttpRequest) {
+          console.log('error', XMLHttpRequest);
+        }
+      });
+    }
   }
 
-  renderData('popular', 'gallery');
+  function filterByID(element, a) {
+    // console.log('in', a);
+    // console.log(element.id);
+    return element.id == a;
+  }
+
+  renderData('#content-holder', 'popular', 'gallery');
   bind();
 });
 //# sourceMappingURL=main.js.map
--- .tmp/scripts/templates.js
+++ .tmp/scripts/templates.js
@@ -9,28 +9,28 @@
           s = this.lambda,
           o = this.escapeExpression,
           r = "function",
-          d = l.helperMissing;
-      return '<iframe class="bg-image" width="100vh" height="100%" src="http://www.youtube.com/embed/' + o(s(null != (t = null != (t = null != (t = null != a ? a.videos : a) ? t.results : t) ? t["0"] : t) ? t.key : t, a)) + '?autoplay=1&controls=0&showinfo=0&loop=1&autohide=1&mute=1" frameborder="0" allowfullscreen autoplay></iframe>\n<div class="box-content">\n		<div class="box-info">\n				<h1>' + o((i = null != (i = l.title || (null != a ? a.title : a)) ? i : d, typeof i === r ? i.call(a, {
+          h = l.helperMissing;
+      return '<iframe class="bg-image" width="100vh" height="100%" src="http://www.youtube.com/embed/' + o(s(null != (t = null != (t = null != (t = null != a ? a.videos : a) ? t.results : t) ? t["0"] : t) ? t.key : t, a)) + '?autoplay=1&controls=0&showinfo=0&loop=1&autohide=1&mute=1" frameborder="0" allowfullscreen autoplay></iframe>\n<div class="box-content">\n		<div class="box-info">\n				<h1>' + o((i = null != (i = l.title || (null != a ? a.title : a)) ? i : h, typeof i === r ? i.call(a, {
         name: "title",
         hash: {},
         data: n
-      }) : i)) + '</h1>\n				<div class="movie-info">\n					<img src="http://image.tmdb.org/t/p/original' + o((i = null != (i = l.poster_path || (null != a ? a.poster_path : a)) ? i : d, typeof i === r ? i.call(a, {
+      }) : i)) + '</h1>\n				<div class="movie-info">\n					<img src="http://image.tmdb.org/t/p/original' + o((i = null != (i = l.poster_path || (null != a ? a.poster_path : a)) ? i : h, typeof i === r ? i.call(a, {
         name: "poster_path",
         hash: {},
         data: n
-      }) : i)) + '" />\n					<div class="small-detail">\n						<h2>' + o((i = null != (i = l.tagline || (null != a ? a.tagline : a)) ? i : d, typeof i === r ? i.call(a, {
+      }) : i)) + '" />\n					<div class="small-detail">\n						<h2>' + o((i = null != (i = l.tagline || (null != a ? a.tagline : a)) ? i : h, typeof i === r ? i.call(a, {
         name: "tagline",
         hash: {},
         data: n
-      }) : i)) + "</h2>\n						<p>runtime : " + o((i = null != (i = l.runtime || (null != a ? a.runtime : a)) ? i : d, typeof i === r ? i.call(a, {
+      }) : i)) + "</h2>\n						<p>runtime : " + o((i = null != (i = l.runtime || (null != a ? a.runtime : a)) ? i : h, typeof i === r ? i.call(a, {
         name: "runtime",
         hash: {},
         data: n
-      }) : i)) + ' minutes</p>\n						<p class="description">' + o((i = null != (i = l.overview || (null != a ? a.overview : a)) ? i : d, typeof i === r ? i.call(a, {
+      }) : i)) + ' minutes</p>\n						<p class="description">' + o((i = null != (i = l.overview || (null != a ? a.overview : a)) ? i : h, typeof i === r ? i.call(a, {
         name: "overview",
         hash: {},
         data: n
-      }) : i)) + '</p>\n						<a class="goBack" data-key=' + o((i = null != (i = l.id || (null != a ? a.id : a)) ? i : d, typeof i === r ? i.call(a, {
+      }) : i)) + '</p>\n						<a class="goBack" data-key=' + o((i = null != (i = l.id || (null != a ? a.id : a)) ? i : h, typeof i === r ? i.call(a, {
         name: "id",
         hash: {},
         data: n
@@ -44,35 +44,31 @@
           i = "function",
           s = l.helperMissing,
           o = this.escapeExpression;
-      return "\n<div style=\"background-image: url('http://image.tmdb.org/t/p/original/" + o((t = null != (t = l.backdrop_path || (null != a ? a.backdrop_path : a)) ? t : s, typeof t === i ? t.call(a, {
-        name: "backdrop_path",
-        hash: {},
-        data: n
-      }) : t)) + '\');">\n	<div class="box-content">\n			<span class="rating">' + o((t = null != (t = l.vote_average || (null != a ? a.vote_average : a)) ? t : s, typeof t === i ? t.call(a, {
+      return '<div class="box-content">\n		<span class="rating">' + o((t = null != (t = l.vote_average || (null != a ? a.vote_average : a)) ? t : s, typeof t === i ? t.call(a, {
         name: "vote_average",
         hash: {},
         data: n
-      }) : t)) + '</span>\n			<div class="box-info">\n					<h1>' + o((t = null != (t = l.title || (null != a ? a.title : a)) ? t : s, typeof t === i ? t.call(a, {
+      }) : t)) + '</span>\n		<div class="box-info">\n				<h1>' + o((t = null != (t = l.title || (null != a ? a.title : a)) ? t : s, typeof t === i ? t.call(a, {
         name: "title",
         hash: {},
         data: n
-      }) : t)) + '</h1>\n\n					<div class="movie-info">\n						<span class="rank">0' + o((l.math || a && a.math || s).call(a, n && n.index, {
+      }) : t)) + '</h1>\n\n				<div class="movie-info">\n					<span class="rank">0' + o((l.math || a && a.math || s).call(a, n && n.index, {
         name: "math",
         hash: {},
         data: n
-      })) + '</span>\n						<div class="small-detail">\n							<p>release date : ' + o((t = null != (t = l.release_date || (null != a ? a.release_date : a)) ? t : s, typeof t === i ? t.call(a, {
+      })) + '</span>\n					<div class="small-detail">\n						<p>release date : ' + o((t = null != (t = l.release_date || (null != a ? a.release_date : a)) ? t : s, typeof t === i ? t.call(a, {
         name: "release_date",
         hash: {},
         data: n
-      }) : t)) + "</p>\n							<p>popularity : " + o((t = null != (t = l.popularity || (null != a ? a.popularity : a)) ? t : s, typeof t === i ? t.call(a, {
+      }) : t)) + "</p>\n						<p>popularity : " + o((t = null != (t = l.popularity || (null != a ? a.popularity : a)) ? t : s, typeof t === i ? t.call(a, {
         name: "popularity",
         hash: {},
         data: n
-      }) : t)) + '</p>\n							<a class="btn" data-key=' + o((t = null != (t = l.id || (null != a ? a.id : a)) ? t : s, typeof t === i ? t.call(a, {
+      }) : t)) + '</p>\n						<a class="btn" data-key=' + o((t = null != (t = l.id || (null != a ? a.id : a)) ? t : s, typeof t === i ? t.call(a, {
         name: "id",
         hash: {},
         data: n
-      }) : t)) + "> Learn More</a>\n						</div>\n					</div>\n			</div>\n	</div>\n</div>\n";
+      }) : t)) + "> Learn More</a>\n					</div>\n				</div>\n		</div>\n</div>\n";
     },
     useData: !0
   }), l["gallery.hbs"] = a({
@@ -82,12 +78,16 @@
           s = "function",
           o = l.helperMissing,
           r = this.escapeExpression,
-          d = '	<!-- <div>p</div> -->\n		<div class="content-box" data-attribute=' + r((i = null != (i = l.id || (null != a ? a.id : a)) ? i : o, typeof i === s ? i.call(a, {
+          h = '	<div class="content-box" data-attribute=' + r((i = null != (i = l.id || (null != a ? a.id : a)) ? i : o, typeof i === s ? i.call(a, {
         name: "id",
         hash: {},
         data: n
-      }) : i)) + " >\n";
-      return t = this.invokePartial(e["gallery-item"], "			", "gallery-item", a, void 0, l, e, n), null != t && (d += t), d + "		</div>\n";
+      }) : i)) + " style=\"background-image: url('http://image.tmdb.org/t/p/original/" + r((i = null != (i = l.backdrop_path || (null != a ? a.backdrop_path : a)) ? i : o, typeof i === s ? i.call(a, {
+        name: "backdrop_path",
+        hash: {},
+        data: n
+      }) : i)) + "');\">\n";
+      return t = this.invokePartial(e["gallery-item"], "		", "gallery-item", a, void 0, l, e, n), null != t && (h += t), h + "	</div>\n";
     },
     compiler: [6, ">= 2.0.0-beta.1"],
     main: function (a, l, e, n) {
